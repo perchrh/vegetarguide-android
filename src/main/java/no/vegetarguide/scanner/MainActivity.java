d1b8442144ab8f68a568e8cee3211d8d6feb8759
Per Christian Henden <perchrh-gh@pvv.org>
2014-02-27 20:59:02 +0100

    Add dummy menu. Use default Android-studio generated resources.

--- src/main/java/no/vegetarguide/scanner/MainActivity.java
+++ src/main/java/no/vegetarguide/scanner/MainActivity.java
@@ -8,6 +8,8 @@ import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
@@ -75,6 +77,26 @@ public class MainActivity extends Activity {
             }
         });
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_about) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     private void startScan() {
